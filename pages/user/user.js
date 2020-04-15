ce11cd1fd724e2d069efdeef48c77d29ddb71634
cwz346852114 <49225483+cwz346852114@users.noreply.github.com>
2020-04-15 18:49:06 +0800

    commit

--- pages/user/user.js
+++ pages/user/user.js
@@ -10,6 +10,10 @@ class Page {
     msg: '',
     height: '',
     userInfo: null,
+    info:{
+      imageURL:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1586957499534&di=3c0fd447b32828d8c0877c35f9c2c39e&imgtype=0&src=http%3A%2F%2Fimg000.hc360.cn%2Fm8%2FM01%2F04%2FE0%2FwKhQplVWs5OEaKF2AAAAAPUPHzk191.jpg",
+      title:"集贤水站(平高世贸店)"
+    }
   }
   onStart() {
 
--- unity/origin/Url.js
+++ unity/origin/Url.js
@@ -1,4 +1,5 @@
 module.exports = {
-    apiUrl: 'http://192.168.0.100:12188/api/mini',//李传浩的本地
+    // apiUrl: 'http://192.168.0.100:12188/api/mini',//李传浩的本地
+    apiUrl: 'http://api.h2o.cy-cube.com/api/mini',//线上
     imgUrl: 'http://192.168.0.100:12188',//李传浩的本地
 }   
