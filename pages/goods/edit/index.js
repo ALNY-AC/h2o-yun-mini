75a035082c003dae7199c0a0975eaf44402e4a9e
cwz346852114 <49225483+cwz346852114@users.noreply.github.com>
2020-04-17 17:38:16 +0800

    commit

--- pages/goods/edit/index.js
+++ pages/goods/edit/index.js
@@ -5,12 +5,21 @@ class Page {
    * 声明data
    */
   data = {
-    form:{
-      name:"",
-      price:0,
-      store_id:2,
-      sort:0
-    }
+    form: {
+      title: "",
+      price: 0,
+      store_id: 2,
+      sort: 0,
+      class_id: ''
+    },
+    show: false,
+    goodsNmae:'',
+    query: {
+      page: 1,
+      page_size: 10,
+      store_id: 2
+    },
+    classList: []
   }
 
 
@@ -36,14 +45,21 @@ class Page {
       const res = await this.$http.post('/store/info', this.data.form);
       if (res.code >= 0) {
         this.setData({
-          form:res.data
+          form: res.data
         })
       }
     }
+    const res1 = await this.$http.post('/class/list', this.data.query);
+    if (res1.code >= 0) {
+      this.setData({
+        classList: res1.data
+      })
+      console.log(this.data.classList)
+    }
   }
-  onChange(e){
+  onChange(e) {
     this.setData({
-      'form.is_up':e.detail
+      'form.is_up': e.detail
     })
   }
   async save() {
@@ -54,6 +70,29 @@ class Page {
     } else {
       this.$toast(res.msg);
     }
+  }
+  showPopup() {
+    this.setData({
+      show: true
+    })
+  }
+  onConfirm(e) {
+
+    this.setData({
+      'form.class_id': e.detail.value.id,
+      goodsNmae: e.detail.value.name,
+      show:false
+    })
+  }
+  onCancel() {
+    this.setData({
+      show: false
+    })
+  }
+  onClose() {
+    this.setData({
+      show: false
+    })
   }
 
 }
