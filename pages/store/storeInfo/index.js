ad9c625485089df9745a6703abe97b9dcd2ea825
代码狮 <1173197065@qq.com>
2020-04-20 15:07:26 +0800

    修改门店详情。添加头像上传，更新有赞框架，更新url的数据

--- pages/store/storeInfo/index.js
+++ pages/store/storeInfo/index.js
@@ -1,38 +1,36 @@
 const origin = require('../../../unity/origin/origin')
+const Upload = require('../../../unity/origin/Upload')
+const File = require('../../../unity/origin/File')
+const computedBehavior = require('miniprogram-computed')
+const Url = require('../../../unity/origin/Url')
+
 
 class Page {
+  behaviors = [computedBehavior]
   /**
    * 声明data
    */
   data = {
-
     form: {
       name: "",//名称
-      y:"",//经度
-      x:"",//纬度
-      info:"",//描述
-      logo:"",//图片
-      contacts:"",//联系方式
-      phone:"",//手机号
-      QQ:""//QQ号
+      y: "",//经度
+      x: "",//纬度
+      info: "",//描述
+      logo: "",//图片
+      contacts: "",//联系方式
+      phone: '',//手机号
+      QQ: ""//QQ号
+    },
+    host: Url.imageUrl
+  }
+  computed = {
+    logo(data) {
+
+      return data.host + data.form.logo;
     }
   }
-
   code = null;
   userInfo = null;
-
-  /**
-   * 监听data数据变化
-   */
-  observers = {
-    msg() {
-      // console.warn('更改');
-      // this.setData({
-      // msg2: this.data.msg.split(' ')[1]
-      // });
-    },
-
-  }
   /**
    * 声明周期函数
    * 在onLoad后立即调用
@@ -42,9 +40,11 @@ class Page {
       const res = await this.$http.post('/store/info', this.data.form);
       if (res.code >= 0) {
         this.setData({
-          form:res.data
+          form: res.data
         })
       }
+    } else {
+      this.setData({ ['form.phone']: wx.getStorageSync('userInfo').phone });
     }
 
   }
@@ -58,6 +58,13 @@ class Page {
     } else {
       this.$toast(res.msg);
     }
+  }
+
+  async upload() {
+    let upload = new Upload(new File());
+    const res = await upload.push();
+    console.warn(res);
+    this.setData({ ['form.logo']: res });
   }
 
 }
