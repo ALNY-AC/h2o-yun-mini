97d15a66d9dc17936bb7e51614f8fa1b8ad28a7f
cwz346852114 <49225483+cwz346852114@users.noreply.github.com>
2020-04-21 20:07:21 +0800

    commit

--- pages/water/list/index.js
+++ pages/water/list/index.js
@@ -5,23 +5,50 @@ class Page {
    * 声明data
    */
   data = {
-
+    list: [],
+    form: {
+      page: 1,
+      page_size: 10,
+    }
   }
 
   /**
    * 声明周期函数
    * 在onLoad后立即调用
    */
-  onStart() {
+  async onStart() {
+    this.setData({
+      'form.store_id': wx.getStorageSync('store_id')
+    });
   }
-
   onShow() {
-
+    this.setData({
+      'form.store_id': wx.getStorageSync('store_id')
+    });
+    this.updateInit();
   }
-
+  //调用接口
   async update() {
-
-
+    const res = await this.$http.post('/water_coupon/list', this.data.form);
+    if (res.code >= 0) {
+      this.setData({
+        list: [...this.data.list, ...res.data.list],
+      })
+    }
+    wx.stopPullDownRefresh();
+  }
+  go(e) {
+    this.$router.push('/pages/goods/edit/index', {
+      id: e.currentTarget.dataset.item.id
+    });
+  }
+  //初始化数据
+  updateInit() {
+    this.setData({
+      list: [],
+      ['form.page']: 1
+    })
+    this.update()
   }
 
 
