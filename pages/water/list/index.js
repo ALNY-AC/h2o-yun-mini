3486ebbd56db9757f460850f714054dd3a90138d
代码狮 <1173197065@qq.com>
2020-04-21 12:02:55 +0800

    修复bug，设置正确的框架路径

--- pages/water/list/index.js
+++ pages/water/list/index.js
@@ -1,4 +1,4 @@
-const origin = require('../../unity/origin/origin')
+const origin = require('../../../unity/origin/origin')
 
 class Page {
   /**
@@ -13,11 +13,10 @@ class Page {
    * 在onLoad后立即调用
    */
   onStart() {
-\
   }
 
   onShow() {
-  
+
   }
 
   async update() {
@@ -25,7 +24,7 @@ class Page {
 
   }
 
- 
+
 
 
 }
