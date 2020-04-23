d92d49cb834a097d7fd90a4b2d2c30470b64d903
代码狮 <1173197065@qq.com>
2020-04-23 17:22:22 +0800

    水票商品选择

--- pages/water/edit/index.js
+++ pages/water/edit/index.js
@@ -14,61 +14,57 @@ class Page {
       price: '',
       store_id: '',
       stock: '',
-      max: '',
       min: '',
       goods_id: []
     },
-    show: false,
-
+    count: 0
   }
   computed = {
     goodsName(data) {
-      return `已选择${data.form.goods_id.selectList.length}个商品`;
+      return `已选择${data.form.goods_id.length}个商品`;
     },
-
   }
 
   onShow() {
-    let list = [];
-    if (wx.getStorageSync('goodsSelectList')) {
-      list = wx.getStorageSync('goodsSelectList')
-    }
     this.setData({
-      ['form.goods_id']: list,
+      count: ++this.data.count,
     });
+    if (this.data.count > 1) {
+      let list = [];
+      if (wx.getStorageSync('goodsSelectList')) {
+        list = wx.getStorageSync('goodsSelectList')
+      }
+      this.setData({
+        ['form.goods_id']: list,
+      });
+    }
+
   }
   /**
    * 声明周期函数
    * 在onLoad后立即调用
    */
   async onStart() {
-    this.data.form.store_id = wx.getStorageSync('store_id');
+
+    this.setData({
+      "form.store_id": wx.getStorageSync('store_id')
+    });
     if (this.$route.query.id) {
       const res = await this.$http.post('/water_coupon/info', {
         id: this.$route.query.id
       });
       if (res.code >= 0) {
         this.setData({
-          form: res.data
+          form: res.data,
         });
       }
-    }
-    this.httpGood();
-  }
-  async httpGood() {
-    const res = await this.$http.post('/goods/list', {
-      store_id: wx.getStorageSync('store_id')
-    });
-    if (res.code >= 0) {
-      this.setData({
-        goodsList: res.data.list
-      });
     }
   }
 
   async save() {
 
     const res = await this.$http.post('/water_coupon/save', this.data.form);
+    wx.setStorageSync('goodsSelectList', []);
     if (res.code >= 0) {
       this.$toast('保存成功');
       this.$router.go(-1);
@@ -76,28 +72,6 @@ class Page {
       this.$toast(res.msg);
     }
   }
-  showPopup() {
-    this.setData({
-      show: true
-    })
-  }
-  onConfirm(e) {
-
-  }
-  onCancel() {
-    this.setData({
-      show: false
-    })
-  }
-  onClose() {
-    this.setData({
-      show: false
-    })
-  }
-
-
-
-
 }
 
 origin(Page)
