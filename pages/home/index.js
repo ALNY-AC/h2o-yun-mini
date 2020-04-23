0b391b1b4d2fbeda6eea5d8428f86038b40a1dc4
wangdan <wangdan9973@163.com>
2020-04-23 18:07:13 +0800

    首页

--- pages/home/index.js
+++ pages/home/index.js
@@ -6,6 +6,33 @@ const computedBehavior = require('miniprogram-computed')
 class Page {
   behaviors = [computedBehavior]
   data = {
+    tool:[
+      {
+        word:'分类管理',
+        url:'',
+        icon:'photo-o',
+      }, {
+        word: '商品管理',
+        url: '',
+        icon: 'photo-o',
+      }, {
+        word: '水票管理',
+        url: '',
+        icon: 'photo-o',
+      }, {
+        word: '订单管理',
+        url: '',
+        icon: 'photo-o',
+      }, {
+        word: '服务配置',
+        url: '',
+        icon: 'photo-o',
+      }, {
+        word: '推广店铺',
+        url: '',
+        icon: 'photo-o',
+      },
+    ]
   }
   computed = {
 
