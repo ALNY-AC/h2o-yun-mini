b2601381705a0cf9c0649f5e85df6167ee5c60b3
sunhui44311 <1874314758@qq.com>
2020-04-27 19:58:16 +0800

    updata

--- pages/user/list/list.js
+++ pages/user/list/list.js
@@ -18,6 +18,32 @@ class Page {
       head: "http://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTK0n3ezUoE4kxjsEUALUw9hYuyUUiawQ0YPaibjtEopzb5Qc3MXhsrSCS1KNabmem7eZXFc1ibssV33A/132",
       name: "PerfectMan Sun",
       price: "0.10",
+    }, {
+      add_time: "2020-04-25 15:21:48",
+      buy_num: 10,
+      goods_id: "[7764,7765,7766]",
+      id: 51,
+      min: 10,
+      name: "桶装水",
+      price: "0.01",
+      stock: 0,
+      store_id: 28,
+      head: "http://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTK0n3ezUoE4kxjsEUALUw9hYuyUUiawQ0YPaibjtEopzb5Qc3MXhsrSCS1KNabmem7eZXFc1ibssV33A/132",
+      name: "PerfectMan Sun",
+      price: "0.10",
+    }, {
+      add_time: "2020-04-25 15:21:48",
+      buy_num: 10,
+      goods_id: "[7764,7765,7766]",
+      id: 51,
+      min: 10,
+      name: "桶装水",
+      price: "0.01",
+      stock: 0,
+      store_id: 28,
+      head: "http://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTK0n3ezUoE4kxjsEUALUw9hYuyUUiawQ0YPaibjtEopzb5Qc3MXhsrSCS1KNabmem7eZXFc1ibssV33A/132",
+      name: "PerfectMan Sun",
+      price: "0.10",
     }]
   }
 
